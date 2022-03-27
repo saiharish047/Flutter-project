a32f23d2010bdd227ab5ba8ba49c40f77f1d73fd
sasharis <sasai.harish@publicissapient.com>
2022-03-27 13:51:18 +0530

    installed expressJS

---
+++ flutter_backend/backend/backend_code/server.js
@@ -0,0 +1,15 @@
+const express = requires('express');
+
+const app = express();
+
+let messageJson = {message:'hello folks'};
+
+app.use(express.json());
+
+app.get('/message',(req,res)=> {
+    res.send(messageJson);
+});
+
+app.listen(3000,()=>{
+    console.log('runnning...');
+});
